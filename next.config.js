044315bd53180ac202984adbede883a7cae38d3c
Rizkifac2204 <rizkifac2204@gmail.com>
2023-01-19 14:10:54 +0700

    dev : install packages required

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_HOST: process.env.HOST,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  },
   reactStrictMode: true,
-}
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
